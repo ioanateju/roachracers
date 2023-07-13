781b488a758f9766258f788db78205c418267d10
Ioana Teju <ixt731@cs.bham.ac.uk>
2023-07-13 12:25:15 +0300

    cloned deepracer-on-the-spot

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,25 +1,33 @@
 def reward_function(params):
     '''
-    Example of rewarding the agent to follow center line
+    Example of penalize steering, which helps mitigate zig-zag behaviors
     '''
     
     # Read input parameters
-    track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    
-    # Calculate 3 markers that are at varying distances away from the center line
+    track_width = params['track_width']
+    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+
+    # Calculate 3 marks that are farther and father away from the center line
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
-    
+
     # Give higher reward if the car is closer to center line and vice versa
     if distance_from_center <= marker_1:
-        reward = 1.0
+        reward = 1
     elif distance_from_center <= marker_2:
         reward = 0.5
     elif distance_from_center <= marker_3:
         reward = 0.1
     else:
         reward = 1e-3  # likely crashed/ close to off track
-    
+
+    # Steering penality threshold, change the number based on your action space setting
+    ABS_STEERING_THRESHOLD = 15
+
+    # Penalize reward if the car is steering too much
+    if steering > ABS_STEERING_THRESHOLD:
+        reward *= 0.8
+
     return float(reward)
