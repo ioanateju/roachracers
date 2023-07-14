b560200095cd07cef97e2815cd4b6b9dfd9ebb1e
Ioana Teju <ixt731@cs.bham.ac.uk>
2023-07-14 17:41:13 +0300

    first try at a more complex reward function - model 1.2.0

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,33 +1,53 @@
+import math
+
+
 def reward_function(params):
-    '''
-    Example of penalize steering, which helps mitigate zig-zag behaviors
-    '''
-    
     # Read input parameters
-    distance_from_center = params['distance_from_center']
     track_width = params['track_width']
-    steering = abs(params['steering_angle']) # Only need the absolute steering angle
+    distance_from_center = params['distance_from_center']
+    all_wheels_on_track = params['all_wheels_on_track']
+    speed = params['speed']
+    progress = params['progress']
+    steps = params['steps']
+    waypoints = params['waypoints']
+    closest_waypoints = params['closest_waypoints']
 
-    # Calculate 3 marks that are farther and father away from the center line
-    marker_1 = 0.1 * track_width
-    marker_2 = 0.25 * track_width
-    marker_3 = 0.5 * track_width
+    # Set constant parameters
+    MAX_STEPS = 50
 
-    # Give higher reward if the car is closer to center line and vice versa
-    if distance_from_center <= marker_1:
-        reward = 1
-    elif distance_from_center <= marker_2:
-        reward = 0.5
-    elif distance_from_center <= marker_3:
-        reward = 0.1
-    else:
-        reward = 1e-3  # likely crashed/ close to off track
+    # Reward weights
+    center_weight = 0.6
+    speed_weight = 0.4
 
-    # Steering penality threshold, change the number based on your action space setting
-    ABS_STEERING_THRESHOLD = 15
+    # Reward for staying close to the centerline
+    reward = 1 - (distance_from_center / (track_width/2 + 0.01))
+    reward = max(reward, 1e-3) * center_weight
 
-    # Penalize reward if the car is steering too much
-    if steering > ABS_STEERING_THRESHOLD:
-        reward *= 0.8
+    # Reward for maintaining speed
+    reward *= speed * speed_weight
+
+    # Penalize if the car goes off track
+    if not all_wheels_on_track:
+        reward = 1e-5
+
+    # Provide a bonus if the car completes a lap
+    if progress == 100:
+        reward += 10
+
+    # Penalize if the car takes too many steps (to encourage efficiency)
+    if steps >= MAX_STEPS:
+        reward = 1e-3
+
+    # Additional reward for following waypoints
+    if closest_waypoints[1] < len(waypoints):
+        next_point = waypoints[closest_waypoints[1]]
+        prev_point = waypoints[closest_waypoints[0]]
+        track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+        car_direction = params['heading']
+        direction_diff = abs(car_direction - track_direction)
+        if direction_diff > math.pi:
+            direction_diff = 2 * math.pi - direction_diff
+        direction_reward = 1 - direction_diff / math.pi
+        reward += 0.3 * direction_reward
 
     return float(reward)
