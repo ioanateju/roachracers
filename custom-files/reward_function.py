ce76aa0c161e9750d93b2283fc1c4cb4da5c3250
Ed Bieda <edward.bieda@gmail.com>
2023-07-23 19:30:48 +0100

    example of using custom waypoints to drive the car with reward function - no waypoints

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,62 +1,47 @@
 import math
 
+# Custom waypoints representing the racing line on the track
+custom_waypoints = [
+    
+]
+
+def get_distance_from_waypoint(x, y, waypoint):
+    # Calculate Euclidean distance between the car and the waypoint
+    dx = x - waypoint[0]
+    dy = y - waypoint[1]
+    return math.sqrt(dx**2 + dy**2)
+
 def reward_function(params):
+    # Define the maximum distance from the racing line to consider the car on track
+    max_waypoint_distance = 0.5
 
-    # Reward weights
-    speed_weight = 100
-    heading_weight = 100
-    steering_weight = 50
+    # Extract relevant information from the parameters
+    x = params['x']
+    y = params['y']
+    speed = params["speed"]
+    progress = params["progress"]
+    all_wheels_on_track = params['all_wheels_on_track']
 
-    # Initialize the reward based on current speed
-    max_speed_reward = 10 * 10
-    min_speed_reward = 2.33 * 2.33
-    abs_speed_reward = params['speed'] * params['speed']
-    speed_reward = (abs_speed_reward - min_speed_reward) / (max_speed_reward - min_speed_reward) * speed_weight
-    
-    # - - - - - 
-    
-    # Penalize if the car goes off track
-    if not params['all_wheels_on_track']:
-        return 1e-3
-    
-    # - - - - - 
+    # Initialize the reward
+    reward = 1e-3
 
-    # Penalize for going slow
-    if not params['speed'] < 3:
-        return 1e-3
+    # Check if all wheels are on the track
+    if not all_wheels_on_track:
+        reward -= 1.0
+        return float(reward)
 
-    # - - - - -
-    
-    # Calculate the direction of the center line based on the closest waypoints
-    next_point = params['waypoints'][params['closest_waypoints'][1]]
-    prev_point = params['waypoints'][params['closest_waypoints'][0]]
+    # Reward for maintaining speed
+    reward *= speed
 
-    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
-    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]) 
-    # Convert to degree
-    track_direction = math.degrees(track_direction)
+        # Provide a bonus if the car completes a lap
+    if progress == 100:
+        reward += 10
 
-    # Calculate the difference between the track direction and the heading direction of the car
-    direction_diff = abs(track_direction - params['heading'])
-    if direction_diff > 180:
-        direction_diff = 360 - direction_diff
-    
-    abs_heading_reward = 1 - (direction_diff / 180.0)
-    heading_reward = abs_heading_reward * heading_weight
-    
-    # - - - - -
-    
-    # Reward if steering angle is aligned with direction difference
-    abs_steering_reward = 1 - (abs(params['steering_angle'] - direction_diff) / 180.0)
-    steering_reward = abs_steering_reward * steering_weight
+    # Calculate the distance from the car to the nearest waypoint
+    closest_distance = min([get_distance_from_waypoint(x, y, waypoint) for waypoint in custom_waypoints])
 
-    # - - - - -
+    # Calculate the reward based on the distance from the racing line
+    if closest_distance < max_waypoint_distance:
+        reward += 1.0 - closest_distance / max_waypoint_distance
 
-    progress_reward = 0
-    # Reward if the car completes a lap
-    if params['progress'] == 100:
-        progress_reward = 10
-
-    # - - - - -
-    
-    return speed_reward + heading_reward + steering_reward + progress_reward
+    return float(reward)
