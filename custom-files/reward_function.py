1a3c148f84f64ee68645c177d18a8ea6f122d7db
Ioana Teju <ixt731@cs.bham.ac.uk>
2023-09-01 13:35:06 +0100

    new model

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -2,51 +2,94 @@ import math
 
 
 def reward_function(params):
-    # Read input parameters
+
+    # waypoint coords
+    # closest way point
+    # coord of self - makes angle more accurate
+    # direction of travel - check if array of waypoints change based on direction
+    # how many waypoints ahead do we want to check - trial / error
+    # get coords of waypoint x ahead
+    # calc angle between closest waypoint and waypoint x ahead
+    # reward based on how close turnng angle is to calc angle
+
     track_width = params['track_width']
     distance_from_center = params['distance_from_center']
-    all_wheels_on_track = params['all_wheels_on_track']
+    steering = abs(params['steering_angle'])
+    direction_stearing=params['steering_angle']
     speed = params['speed']
-    progress = params['progress']
     steps = params['steps']
+    is_offtrack=params['is_offtrack']
+    progress = params['progress']
+    all_wheels_on_track = params['all_wheels_on_track']
+    x = params['x']
+    y = params['y']
+
+    SPEED_THRESHOLD_2=1.8
+    DIRECTION_THRESHOLD = 3.0
+    SPEED_THRESHOLD_1=3.5
+    # Read input variables
+
     waypoints = params['waypoints']
     closest_waypoints = params['closest_waypoints']
+    heading = params['heading']
+    benchmark_time=14.2
+    benchmark_steps=173
+    straight_waypoints=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170]
 
-    # Set constant parameters
-    MAX_STEPS = 80
 
-    # Reward weights
-    center_weight = 0.65
-
-    # Reward for staying close to the centerline
-    reward = 1 - (distance_from_center / (track_width/2 + 0.01))
-    reward = max(reward, 1e-3) * center_weight
-
-    # Reward for maintaining speed
-    reward *= speed
-
-    # Penalize if the car goes off track
-    if not all_wheels_on_track:
-        reward = 1e-5
-
-    # Provide a bonus if the car completes a lap
+    # Get reward if completes the lap and more reward if it is faster than benchmark_time    
     if progress == 100:
-        reward += 10
+        if round(steps/15,1)<benchmark_time:
+            reward+=100*round(steps/15,1)/benchmark_time
+        else:
+            reward += 100
+    elif is_offtrack:
+        reward-=50   
+    
+    # Calculate the direction of the center line based on the closest waypoints
 
-    # Penalize if the car takes too many steps (to encourage efficiency)
-    if steps >= MAX_STEPS:
-        reward = 1e-3
+    next_point = waypoints[closest_waypoints[1]]
+    prev_point = waypoints[closest_waypoints[0]]
 
-    # Additional reward for following waypoints
-    if closest_waypoints[1] < len(waypoints):
-        next_point = waypoints[closest_waypoints[1]]
-        prev_point = waypoints[closest_waypoints[0]]
-        track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
-        car_direction = params['heading']
-        direction_diff = abs(car_direction - track_direction)
-        if direction_diff > math.pi:
-            direction_diff = 2 * math.pi - direction_diff
-        direction_reward = 1 - direction_diff / math.pi
-        reward += 0.9 * direction_reward
+    # Calculate the direction in radius, arctan2(dy, dx), the result is (-pi, pi) in radians
 
-    return float(reward)
+    track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0]) 
+
+    # Convert to degree
+
+    track_direction = math.degrees(track_direction)
+
+    # Calculate the difference between the track direction and the heading direction of the car
+
+    direction_diff = abs(track_direction - heading)
+
+    # Penalize the reward if the difference is too large
+   
+    direction_bonus=1
+  
+    if direction_diff > DIRECTION_THRESHOLD or not all_wheels_on_track:
+
+        direction_bonus=1-(direction_diff/15)
+        if direction_bonus<0 or direction_bonus>1:
+            direction_bonus = 0
+        reward *= direction_bonus
+    else:
+        if next_point in (straight_waypoints):
+            if speed>=SPEED_THRESHOLD_1:
+                reward+=max(speed,SPEED_THRESHOLD_1)
+            else:
+                reward+=1e-3
+        else:
+            if speed<=SPEED_THRESHOLD_2:
+                reward+=max(speed,SPEED_THRESHOLD_2)
+            else:
+                reward+=1e-3
+    
+    # Give additional reward if the car pass every 50 steps faster than expected
+    if (steps % 50) == 0 and progress >= (steps / benchmark_steps) * 100 :
+        reward += 10.0
+    # Penalize if the car cannot finish the track in less than benchmark_steps
+    elif (steps % 50) == 0 and progress < (steps / benchmark_steps) * 100 :
+        reward-=5.0
+    return reward
+
